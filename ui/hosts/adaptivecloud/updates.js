4cf364221e2b581b1429a36d1c2fc2d9adbfc3ce
Sean Lair <slair@ippathways.com>
2020-01-31 17:34:06 +0000

    Branded with AdaptiveCloud and support for white-labeling

--- ui/hosts/adaptivecloud/updates.js
+++ ui/hosts/adaptivecloud/updates.js
@@ -1,12 +1,12 @@
 export function addElements() {
-	$('<link rel="apple-touch-icon" sizes="180x180" href="hosts/gregmbp/apple-touch-icon.png">').appendTo('head')
-	$('<link rel="icon" type="image/png" sizes="32x32" href="hosts/gregmbp/favicon-32x32.png">').appendTo('head')
-	$('<link rel="icon" type="image/png" sizes="16x16" href="hosts/gregmbp/favicon-16x16.png">').appendTo('head')
-	$('<link rel="manifest" href="hosts/gregmbp/site.webmanifest">').appendTo('head')
-	$('<link rel="mask-icon" href="hosts/gregmbp/safari-pinned-tab.svg" color="#5bbad5">').appendTo('head')
-	$('<link rel="shortcut icon" href="hosts/gregmbp/favicon.ico">').appendTo('head')
+	$('<link rel="apple-touch-icon" sizes="180x180" href="hosts/adaptivecloud/apple-touch-icon.png">').appendTo('head')
+	$('<link rel="icon" type="image/png" sizes="32x32" href="hosts/adaptivecloud/favicon-32x32.png">').appendTo('head')
+	$('<link rel="icon" type="image/png" sizes="16x16" href="hosts/adaptivecloud/favicon-16x16.png">').appendTo('head')
+	$('<link rel="manifest" href="hosts/adaptivecloud/site.webmanifest">').appendTo('head')
+	$('<link rel="mask-icon" href="hosts/adaptivecloud/safari-pinned-tab.svg" color="#5bbad5">').appendTo('head')
+	$('<link rel="shortcut icon" href="hosts/adaptivecloud/favicon.ico">').appendTo('head')
 	$('<meta name="msapplication-TileColor" content="#da532c">').appendTo('head')
-	$('<meta name="msapplication-config" content="hosts/gregmbp/browserconfig.xml">').appendTo('head')
+	$('<meta name="msapplication-config" content="hosts/adaptivecloud/browserconfig.xml">').appendTo('head')
 	$('<meta name="theme-color" content="#ffffff">').appendTo('head')
 }
 export function removeOverlapping() {
