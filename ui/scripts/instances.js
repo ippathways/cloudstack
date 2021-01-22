deb51567390292b2d84e87968034f6eca9cc61e1
Sean Lair <slair@ippathways.com>
2021-01-22 09:20:05 -0600

    Sort accounts in search dialog for Instances tab

--- ui/scripts/instances.js
+++ ui/scripts/instances.js
@@ -468,7 +468,9 @@
                                             description: this.name
                                         });
                                     })
-
+                                    items.sort(function(a, b) {
+                                        return a.description.localeCompare(b.description);
+                                    });
                                     args.response.success({
                                         data: items
                                     });
